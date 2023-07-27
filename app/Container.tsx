5f7a120b3c59d4148112e3b3d0f288d11091ac5c
Subhangi Dhasmana <91680044+SUBHANGI-DHASMANA@users.noreply.github.com>
2023-07-27 22:52:38 +0530

    Minor changes

--- app/Container.tsx
+++ app/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AiOutlineTwitter, AiOutlineGithub } from "react-icons/ai"
 import { ImLinkedin } from "react-icons/im"
-import { FiDribbble } from "react-icons/fi"
+import Image from 'next/image';
 import "./App.css"
 
 
@@ -15,19 +15,19 @@ const Container = (props: Props) => {
                     <div className='relative'>
                         {/* <img className=' w-100% h-auto' width={600} src="pic1.png" alt="" />
                         <div className='w-[50%] overflow-hidden h-[90%] z-[-1] top-[0%] left-[25%] rounded-full bg-gradient-to-r from-slate-600 via-slate-500 to-slate-400  absolute'></div> */}
-                        <img className=' w-100% h-auto transform -scale-x-100' width={350} src="pic2.png" alt="" />
+                        <Image className=' w-100% h-auto transform -scale-x-100' width={350} src="pic2.png" alt="" />
                         <div className='w-[100%] overflow-hidden h-[70%] z-[-1] top-[20%] left-[0%] rounded-full  absolute' style={{backgroundImage: "linear-gradient(to right, #9796f0, #fbc7d4)"}}></div>
                     </div>
                 </div>
                 <div className='p-4 pl-0 mt-5 sm:mt-20'>
-                    <p className='text-xl sm:text-2xl  md:text-xl lg:text-2xl xl:text-3xl'>Hello! I'm</p>
+                    <p className='text-xl sm:text-2xl  md:text-xl lg:text-2xl xl:text-3xl'>Hello! I&apos;m</p>
                     <h1 className='text-4xl sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl pt-2 pb-3  lg:pt-6'>SUBHANGI DHASMANA</h1>
                     <section className="animation text-4xl">
                         <div className="first"><div className='w-full py-5'>DEVELOPER</div></div>
                         <div className="second"><div className='w-full py-5'>DEVELOPER</div></div>
                         {/* <div class="third"><div className='w-full my-5'>WEB DESIGNER</div></div> */}
                     </section>
-                    <p className='text-base sm:text-xl md:text-base lg:text-xl xl:text-2xl lg:pt-6'>The data doesn't lie, but it doesn't always <br/> tell the truth.</p>
+                    <p className='text-base sm:text-xl md:text-base lg:text-xl xl:text-2xl lg:pt-6'>The data doesn&apos;t lie, but it doesn&apos;t always <br/> tell the truth.</p>
                     <div className='flex items-center mt-6 md:mt-14'>
                         <ul className='flex items-center space-x-2 sm:space-x-5 mr-6 my-4 '>
                             <li><a href="https://twitter.com/SubhangiDhas" target='_blank' className='hover:opacity-100 opacity-75 transition ease-in duration-150'><AiOutlineTwitter size={24} /></a></li>
--- app/Projects.tsx
+++ app/Projects.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image';
 import React, {useState} from 'react'
 import {BsArrowUpRight} from "react-icons/bs"
 
@@ -11,7 +12,7 @@ const Projects = (props: Props) => {
         <h1 className='md:text-4xl text-3xl lg:text-5xl font-bold mb-10 pb-3 border-b-[1px] border-neutral-500'>Projects</h1>
         {/* <div className='w-10/12 box1 max-md:mt-[-50px] mt-0'></div> */}
         <div className='mt-20 md:w-9/12 mx-auto '>
-            <img className='rounded-xl opacity-90' src="img1.png" alt="" />
+        <Image className='rounded-xl opacity-90' src="img1.png" alt="" />
             <div className='my-10 sm:w-10/12 mx-auto'>
                 <h1 className='text-2xl font-bold mb-4 text-[#C9D1D8]'>Gas Concentration Time Series Prediction</h1>
                 <div className='text-neutral-300'>This app predict the value of concentration based on the input you give.</div>
@@ -19,7 +20,7 @@ const Projects = (props: Props) => {
             </div>
         </div>
         <div className='mt-20 md:w-9/12 mx-auto '>
-            <img className='rounded-xl opacity-90' src="img2.png" alt="" />
+        <Image className='rounded-xl opacity-90' src="img2.png" alt="" />
             <div className='my-10 sm:w-10/12 mx-auto'>
                 <h1 className='text-2xl font-bold mb-4 text-[#1D7A7F]'>Online Transaction Website</h1>
                 <div className='text-neutral-300'>The full-stack banking website built with the MERN stack enables users to access their past transactions, view detailed transaction history, and transfer money securely. It integrates MongoDB for data storage, Express for server-side logic, React for dynamic user interfaces, and Node.js for smooth functionality.</div>
@@ -27,7 +28,7 @@ const Projects = (props: Props) => {
             </div>
         </div>
         <div className='mt-20 md:w-9/12 mx-auto '>
-            <img className='rounded-xl opacity-90' src="img3.png" alt="" />
+        <Image className='rounded-xl opacity-90' src="img3.png" alt="" />
             <div className='my-10 sm:w-10/12 mx-auto'>
                 <h1 className='text-2xl font-bold mb-4 text-[#DF6C27]'>E-commerce Website</h1>
                 <div className='text-neutral-300'>The frontend of the e-commerce app for book sales, developed using JavaScript, offers a user-friendly interface with dynamic book listings, search filters, secure payment gateways, and interactive features to enhance the shopping experience, making it a seamless platform for book enthusiasts.</div>
